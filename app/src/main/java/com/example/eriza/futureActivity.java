ed1f92b4faf683379d7e9320600ce3818a0d372a
Hunajpu <34669573+Hunajpu@users.noreply.github.com>
2020-11-09 23:18:08 -0600

    Corrección de error en strings y creación de las actividades futuro y pasado

---
+++ app/src/main/java/com/example/eriza/futureActivity.java
@@ -0,0 +1,14 @@
+package com.example.eriza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class futureActivity extends userActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_future);
+    }
+}
--- app/src/main/java/com/example/eriza/userActivity.java
+++ app/src/main/java/com/example/eriza/userActivity.java
@@ -2,14 +2,46 @@ package com.example.eriza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class userActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //comentario
         setContentView(R.layout.activity_user);
+
+        //Botón que te lleva a la pestaña de historial
+        final Button botonHistorial = findViewById(R.id.button3);
+        botonHistorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirHistorial();
+            }
+        });
+
+        //Botón que te lleva a la pestaña de predicciones
+        final Button botonFuturo = findViewById(R.id.button4);
+        botonHistorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirFuturo();
+            }
+        });
+    }
+
+    public void abrirHistorial()
+    {
+        Intent intent = new Intent(this, pastActivity.class);
+        startActivity(intent);
+    }
+
+    public void abrirFuturo()
+    {
+        Intent intent = new Intent(this, futureActivity.class);
+        startActivity(intent);
     }
 }
