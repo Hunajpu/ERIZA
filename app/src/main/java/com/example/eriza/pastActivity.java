bbdcb2e42725b23ffa1b63e419f67de931046a24
Hunajpu <34669573+Hunajpu@users.noreply.github.com>
2020-11-11 00:02:56 -0600

    Se creo función fetchInfo
    Una forma de explicar como se conectaria con firebase la aplicación mostrando ekl útlimo valor de temperatura

--- app/src/main/java/com/example/eriza/pastActivity.java
+++ app/src/main/java/com/example/eriza/pastActivity.java
@@ -1,19 +1,68 @@
 package com.example.eriza;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
 
 public class pastActivity extends AppCompatActivity {
 
-    //private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "pastActivity";
+    public static final String TEMP_KEY = "temp";
+    public static final String DATE_KEY = "date";
+    TextView tx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past);
+
+        tx = findViewById(R.id.textView6);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchInfo();
+            }
+        });
+    }
+
+    public void fetchInfo()
+    {
+        Log.d(TAG,"Boton Presionado");
+        DocumentReference docRef = db.collection("temperatura").document("t1");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                    tx.setText("Último valor de temperatura: "+document.getDouble(TEMP_KEY).toString()+"°C. Fecha: "+document.getTimestamp(DATE_KEY).toDate());
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
 }
