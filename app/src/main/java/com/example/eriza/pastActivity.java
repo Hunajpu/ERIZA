f83258c7ef817a55272e2196fbd217a1024197bd
Hunajpu <34669573+Hunajpu@users.noreply.github.com>
2020-11-10 18:23:44 -0600

    Creación de base de datos

--- app/src/main/java/com/example/eriza/pastActivity.java
+++ app/src/main/java/com/example/eriza/pastActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class pastActivity extends AppCompatActivity {
+
+    //private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
